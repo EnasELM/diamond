8ee1cfc8505b844c710106ab739db166c0a40de6
Enas <enaselmahdwi@gmail.com>
2022-06-20 09:37:14 -0600

    up

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,9 +1,9 @@
 import Commerce from "@chec/commerce.js";
 
-// export const commerce = new Commerce(
-//     "pk_40231f204b78f4c4ec50a109d97a2facada1b95cdc3c2"
-// );
-// export const commerce = new Commerce("pk_34564fe0feb8c44ebbb53da5b2383587b193517710a52")
 export const commerce = new Commerce(
-    "pk_43165724aa684fa921fa0618ec1fe1404db10adfd60b3"
+    "pk_40231f204b78f4c4ec50a109d97a2facada1b95cdc3c2"
 );
+// export const commerce = new Commerce("pk_34564fe0feb8c44ebbb53da5b2383587b193517710a52")
+// export const commerce = new Commerce(
+//     "pk_43165724aa684fa921fa0618ec1fe1404db10adfd60b3"
+// );
